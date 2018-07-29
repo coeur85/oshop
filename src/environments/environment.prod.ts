2ef4b447d7c9fbb67fc862f149b2a73df1a950a5
coeur85 <34419909+coeur85@users.noreply.github.com>
2018-07-29 18:45:30 +0200

    Verion 0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyALQI5usm7VOfTUcJAIRoJw300kzuNx7Vw',
+    authDomain: 'angular-oshop-34aa5.firebaseapp.com',
+    databaseURL: 'https://angular-oshop-34aa5.firebaseio.com',
+    projectId: 'angular-oshop-34aa5',
+    storageBucket: '',
+    messagingSenderId: '794928116451'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyALQI5usm7VOfTUcJAIRoJw300kzuNx7Vw',
+    authDomain: 'angular-oshop-34aa5.firebaseapp.com',
+    databaseURL: 'https://angular-oshop-34aa5.firebaseio.com',
+    projectId: 'angular-oshop-34aa5',
+    storageBucket: '',
+    messagingSenderId: '794928116451'
+  }
 };
 
 /*
